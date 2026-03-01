113a78d65a56f8c5aa26e698e46a877fb4a82e4b
Doshtik <igor.lizyakinvladislavovic@gmail.com>
2026-03-02 02:27:16 +0500

    Добавил темы и локализацию Внедрил темы Не внедрил локадизацию

--- Desktop_Amethyst_Audio/ViewModels/AuthViewModel.cs
+++ Desktop_Amethyst_Audio/ViewModels/AuthViewModel.cs
@@ -1,8 +1,39 @@
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Markup;
 using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Desktop_Amethyst_Audio.ViewModels;
 
 public class AuthViewModel : ObservableObject
 {
+    public void ChangeTheme(string themeName)
+    {
+        Uri uri = new Uri($"Themes/Theme.{themeName}.xaml", UriKind.Relative);
+        ResourceDictionary newTheme = new ResourceDictionary { Source = uri };
+        Collection<ResourceDictionary> dictionaries = Application.Current.Resources.MergedDictionaries;
+        for (int i = 0; i < dictionaries.Count; i++)
+        {
+            if (dictionaries[i].Source.OriginalString.Contains("Theme."))
+            {
+                dictionaries.RemoveAt(i);
+                dictionaries.Insert(i, newTheme);
+                return;
+            }
+        }
+        dictionaries.Add(newTheme);
+    }
     
+    public void ChangeLanguage(string cultureCode)
+    {
+        var culture = new CultureInfo(cultureCode);
+    
+        Thread.CurrentThread.CurrentCulture = culture;
+        Thread.CurrentThread.CurrentUICulture = culture;
+    
+        FrameworkElement.LanguageProperty.OverrideMetadata(
+            typeof(FrameworkElement),
+            new FrameworkPropertyMetadata(XmlLanguage.GetLanguage(culture.IetfLanguageTag)));
+    }
 }

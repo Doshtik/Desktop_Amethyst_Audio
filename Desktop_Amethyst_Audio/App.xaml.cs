b6f4128351c28aa5ed3c0b72583caa81d793727d
Doshtik <igor.lizyakinvladislavovic@gmail.com>
2026-03-03 21:00:41 +0500

    Вынес стили кнопки в отдельный файл TODO: Вынести цвета из стиля в тему

--- Desktop_Amethyst_Audio/App.xaml.cs
+++ Desktop_Amethyst_Audio/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using Desktop_Amethyst_Audio.Views.Windows;
 
 namespace Desktop_Amethyst_Audio;
 
@@ -9,4 +10,33 @@ namespace Desktop_Amethyst_Audio;
 /// </summary>
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        // 1. Твоя логика проверки (например, через сервис или настройки)
+        bool isAuthenticated = true; //CheckUserAuth(); 
+
+        if (isAuthenticated)
+        {
+            // Показываем основное окно
+            new LayoutWindow().Show();
+        }
+        else
+        {
+            // Показываем окно входа
+            var loginWindow = new AuthWindow();
+        
+            // Если нужно открыть главное окно ПОСЛЕ успешного входа:
+            if (loginWindow.ShowDialog() == true) 
+            {
+                new LayoutWindow().Show();
+            }
+            else
+            {
+                // Если пользователь закрыл окно входа, закрываем приложение
+                Shutdown();
+            }
+        }
+    }
 }
